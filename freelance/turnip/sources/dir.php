55ec86816ef028def508590562a8be78297745f3
Cecchi MacNaughton <cecchi.macnaughton@gmail.com>
2012-06-17 17:02:00 -0400

    Turnip: Fixed slide sorting, adding comments

--- freelance/turnip/sources/dir.php
+++ freelance/turnip/sources/dir.php
@@ -1,14 +1,27 @@
 <?php
+/* 
+ * All directory requests within "/sources" are routed through this script,
+ * and the contents (slide images) are output as a JSON array
+ */
+
+// Open the directory
 if($handle = opendir($_GET['path'])) {
   $files = array();
+  // Iterate through the files
   while(false !== ($entry = readdir($handle))) {
+    // Ignore all files but .png and .jp(e)g
     if(preg_match('%^\d+\.(png|jpe?g)$%', $entry)) {
       array_push($files, $entry);
     }
   }
 
-  echo json_encode(sort($files));
+  // Sort the slides numerically
+  sort($files);
 
+  // Output as a simple JSON array
+  echo json_encode($files);
+
+  // Close the directory handle
   closedir($handle);
 }
 ?>
